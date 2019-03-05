278fecde32582a15968a14b8b64980d9932ab445
quannk <quannk@coccoc.com>
2019-03-05 17:32:47 +0700

    add template main.yaml

---
+++ src/main.yaml.ex
@@ -0,0 +1,10 @@
+# fill your api key here and rename/copy main.yaml.ex to main.yaml to make it works
+api_key: <fill your api key here>
+
+# folder which contains raw data
+storage: /etc/coccoc-gsb4/data/
+
+# folder which contains extracted data so human and java can read easily
+output: /etc/coccoc-gsb4/extract/
+
+lock_file: /tmp/coccoc.gsb4.lock
